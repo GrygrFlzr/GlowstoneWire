9c6fdcd5962e846ca96d799a8ee59562aec86b87
GrygrFlzr <grygrflzr@hotmail.com>
2014-01-10 19:46:19 +0800

    Pushed version build number

--- grygrflzr/mods/glowstonewire/GlowstoneWireMod.java
+++ grygrflzr/mods/glowstonewire/GlowstoneWireMod.java
@@ -16,7 +16,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class GlowstoneWireMod {
     public static final String MODID = "GrygrFlzr_GlowstoneWire";
     public static final String NAME = "Glowstone Wire";
-    public static final String VERSION = "build 111";
+    public static final String VERSION = "build 112";
     
     public static Block glowstoneWire;
     public static int gsWireColorR = 255;
