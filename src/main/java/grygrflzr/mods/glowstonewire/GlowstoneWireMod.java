3a28cf591bc62f5e5c071096d322f4fc44d90a3e
GrygrFlzr <grygrflzr@hotmail.com>
2014-07-03 13:33:21 +0800

    Version bump to 1.0.2.113

--- src/main/java/grygrflzr/mods/glowstonewire/GlowstoneWireMod.java
+++ src/main/java/grygrflzr/mods/glowstonewire/GlowstoneWireMod.java
@@ -17,7 +17,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class GlowstoneWireMod {
     public static final String MODID = "GrygrFlzr_GlowstoneWire";
     public static final String NAME = "Glowstone Wire";
-    public static final String VERSION = "1.0.1.112";
+    public static final String VERSION = "1.0.2.113";
     
     public static Block glowstoneWire;
     public static int gsWireColorR = 255;
